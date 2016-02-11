dd3f7d5dea2e71ff9372868b0ba19d4b70d00dfd
Musonick58 <nik894@gmail.com>
2016-02-11 17:28:34 +0100

    Aggiunte due classi di Dati per la app

---
+++ TheLastStop/app/src/main/java/andoridserver/androidData/AndroidDataLinee.java
@@ -0,0 +1,28 @@
+package andoridserver.androidData;
+
+import java.util.List;
+
+/**
+ * Created by nichi on 11/02/2016.
+ */
+public class AndroidDataLinee implements AndroidDataInterface {
+    @Override
+    public String getNameObject() {
+        return null;
+    }
+
+    @Override
+    public List<String> getDataAsList() {
+        return null;
+    }
+
+    @Override
+    public void addData(String dato) {
+
+    }
+
+    @Override
+    public void setData(String s, int index) {
+
+    }
+}
