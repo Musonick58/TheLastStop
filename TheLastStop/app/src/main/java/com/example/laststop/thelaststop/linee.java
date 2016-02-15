9fb2acc884fa24c54b07eccd9e9c9e4585fa888b
michelemnt <Michele@MacBook-Pro-di-Michele-3.local>
2016-02-15 15:17:05 +0100

    Portrait FIxed

--- TheLastStop/app/src/main/java/com/example/laststop/thelaststop/linee.java
+++ TheLastStop/app/src/main/java/com/example/laststop/thelaststop/linee.java
@@ -8,7 +8,6 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class linee extends ActionBarActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,12 +15,14 @@ public class linee extends ActionBarActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         String trasporto = getIntent().getExtras().getString("Trasporto");
         setTitle("Linee " + trasporto);
-        List<String> lines = new ArrayList<>();
 
+        if (trasporto.equals("Bus")){
+
+        }
+
+        List<String> lines = new ArrayList<>();
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, lines);
         ListView listView = (ListView) findViewById(R.id.listaLinee);
         listView.setAdapter(adapter);
-
     }
-
 }
