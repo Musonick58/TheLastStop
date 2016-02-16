d67f6a70245430a5d9644276066d2cd5da408850
michelemnt <Michele@MacBook-Pro-di-Michele-3.local>
2016-02-16 14:52:37 +0100

    Update multicolumnList

---
+++ TheLastStop/app/src/main/java/com/example/laststop/thelaststop/constant.java
@@ -0,0 +1,10 @@
+package com.example.laststop.thelaststop;
+
+/**
+ * Created by Michele on 16/02/16.
+ */
+public class constant {
+    public static final String FIRST_COLUMN = "Ora Arrivo";
+    public static final String SECOND_COLUMN = "Ora Partenza";
+    public static final String THIRD_COLUMN = "Ritardo";
+}
