2c0eb89c4163c175716537178c9a63e4e5ff21b0
michelemnt <Michele@MacBook-Pro-di-Michele-5.local>
2016-02-25 17:31:12 +0100

    onBackPressed

--- TheLastStop/app/src/main/java/com/example/laststop/thelaststop/orari.java
+++ TheLastStop/app/src/main/java/com/example/laststop/thelaststop/orari.java
@@ -14,6 +14,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
@@ -179,6 +180,7 @@ public class orari extends ActionBarActivity {
             list.add(temp);
         }
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         Log.d("metodo","Sono entrato su onOptionsItemSelected di orari.java");
