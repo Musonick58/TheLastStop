dea3f8b19abc3d894b33439627a4532e5489455b
JWMink <johnmink89@hotmail.it>
2016-02-25 15:31:43 +0100

    UDC

--- TheLastStop/app/src/main/java/com/example/laststop/thelaststop/segnalazione.java
+++ TheLastStop/app/src/main/java/com/example/laststop/thelaststop/segnalazione.java
@@ -48,8 +48,8 @@ public class segnalazione extends ActionBarActivity {
                 asd.execute(richiesta);
                 try{
                     if(asd.get().get(0).equals("nothing to send")) {
-                        StackPointerContainer.getInstance().getMainPointer().popup(StackPointerContainer.getInstance().getSegnalazionePointer(), "Segnalazione "+systemTime().substring(0, 4)+" Inviata", "Ti ringraziamo per il tuo tempo...sei un grande!");
-                        Log.d("ritardo", "Segnalato ritardo "+systemTime());
+                        StackPointerContainer.getInstance().getMainPointer().popup(StackPointerContainer.getInstance().getSegnalazionePointer(), "Segnalazione "+ritardo.substring(0, 5)+" Inviata", "Ti ringraziamo per il tuo tempo...sei un grande!");
+                        Log.d("ritardo", "Segnalato ritardo "+ ritardo);
                     }
                     else
                         StackPointerContainer.getInstance().getMainPointer().popup(StackPointerContainer.getInstance().getSegnalazionePointer(), costanti.CON_SERVER_ERR_MSG,costanti.CON_TOAST_ERR_MSG);
